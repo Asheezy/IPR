a5dc05013ff31088ccde286a2cfde1eda1fef407
Kungurov_Max <maxforev@gmail.com>
2022-09-06 06:24:08 +0300

    Added code for task

---
+++ Task/Program.cs
@@ -0,0 +1,30 @@
+﻿void TransformToLength3Less(string[] initArr, string[] finArr)
+{
+    int count = 0;
+    for (int i = 0; i < initArr.Length; i++)
+    {
+        if (initArr[i].Length <= 3)
+        {
+            finArr[count] = initArr[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите элементы через пробел: ");
+var initialArray = Console.ReadLine()
+                            .Split()
+                            .ToArray();
+string[] finalArray = new string[initialArray.Length];
+
+TransformToLength3Less(initialArray, finalArray);
+PrintArray(finalArray);
